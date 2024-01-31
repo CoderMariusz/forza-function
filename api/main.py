3aa65d11a7e7fb14149327db7791b9d32e6beb06
Mariusz Krawczyk <przyslony@gmail.com>
2024-01-31 20:04:28 +0000

    change roots add function send json

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return "Hello, World!"
+@app.route('/json', methods=['GET'])
+def send_json():
+    json = {"h": "haaaloo"}
+    return json
